a3aea5bf22e968bd151c470ac04e23c2a6228c36
mowslasher83 <radeswaled@hotmail.com>
2025-08-31 03:11:32 -0500

    feat: initial commit for src/App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Typography } from './index';
+import './style.css';
+
+const App = () => {
+  return (
+    <div>
+      <Typography variant="h1">Chào mừng đến với hệ thống thiết kế kiểu dáng chữ!</Typography>
+      <Typography variant="body">Đây là một ví dụ đơn giản để bắt đầu.</Typography>
+    </div>
+  );
+};
+
+export default App;
